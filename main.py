29ba5a627b1bd9b3270b462774b10fd50b54e8a2
9zv6vrj42g-sketch <9zv6vrj42g@privaterelay.appleid.com>
2026-07-13 01:40:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from aiogram.enums import ParseMode
 
 from config import settings
 from database.engine import init_db
-from handlers.admin import actual_notsend, messaging, raffle_admin, stats
+from handlers.admin import actual_notsend, images, messaging, raffle_admin, stats
 from handlers.moderation import mod_queue
-from handlers.user import actual, content, notifications, raffle, start, suggest
+from handlers.user import actual, content, notifications, raffle, share, start, suggest
 from middlewares.access import AccessControlMiddleware, PrivateOnlyMiddleware
 from utils.scheduler import setup_scheduler
 
@@ -30,10 +30,12 @@ async def main() -> None:
     dp.include_router(stats.router)
     dp.include_router(raffle_admin.router)
     dp.include_router(actual_notsend.router)
+    dp.include_router(images.router)
     dp.include_router(mod_queue.router)
 
     dp.include_router(start.router)
     dp.include_router(content.router)
+    dp.include_router(share.router)
     dp.include_router(suggest.router)
     dp.include_router(raffle.router)
     dp.include_router(actual.router)
